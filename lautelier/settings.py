8c24c50897fab58b884313451ca1442590434cd5
Antonio-dev7 <antonio.emk28@gmail.com>
2020-12-13 00:11:40 +0000

    email bug fixed

--- lautelier/settings.py
+++ lautelier/settings.py
@@ -208,7 +208,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'lautelier.@example.com'
+    DEFAULT_FROM_EMAIL = 'lautelier@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
